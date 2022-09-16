332c8f0626e3183359e19c052e4f056f75a9f9ff
wilocu <matpat17@gmail.com>
2022-09-16 10:55:36 -0400

    Model added

---
+++ src/main/java/exeptions/NotConnectedExeption.java
@@ -0,0 +1,8 @@
+package exeptions;
+
+
+public class NotConnectedExeption extends Exception {
+    public NotConnectedExeption(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/people/Address.java
@@ -0,0 +1,89 @@
+package people;
+
+import java.util.Objects;
+import org.apache.log4j.Logger;
+
+public class Address {
+    private static final Logger LOGGER = Logger.getLogger(Address.class.getName());
+    //Members
+    private int id;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private int zipcode;
+
+
+    //Constructors
+    public Address() {
+    }
+    public Address(String saddress, String c, String s, int zip) {
+        streetAddress = saddress;
+        city = c;
+        state = s;
+        zipcode = zip;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int newID) {
+        id = newID;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String newCity) {
+        city = newCity;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zip) {
+        zipcode = zip;
+    }
+
+    //Class Overrides
+    @Override
+    public String toString() {
+        return streetAddress + ", " + city + ", " + state + ", " + zipcode + ", ";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Address)) return false;
+        Address address = (Address) obj;
+
+        return (Objects.equals(this.streetAddress, address.streetAddress) &&
+                Objects.equals(this.city, address.city) &&
+                Objects.equals(this.state, address.state) &&
+                this.zipcode == address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(streetAddress, city, state, zipcode);
+    }
+}
